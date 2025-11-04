9304ffd8184874a9de7076f25a8b314faed2f637
Adrian351lang <177791808+Adrian351lang@users.noreply.github.com>
2025-11-04 20:21:56 -0300

    feat: added health and score fields to knight character

--- game/core/src/main/java/io/github/gamification/Knight.java
+++ game/core/src/main/java/io/github/gamification/Knight.java
@@ -1,7 +1,12 @@
 package io.github.gamification;
 
 public class Knight extends Character {
+	private int health;
+	private int score;
+
 	public Knight(float centerX, float centerY) {
-		super(centerX, centerY, 128, 128, "knight", 10);
+		super(centerX, centerY, 128, 128, "knight", 10); // creates a new 128x128 character with the "knight" animation sprite with 10 frames
+		this.health = 5; // I decided to make his initial health as 5; can be changed just fine
+		this.score = 0; // will be added to 1 for every question answered right
 	}
 }
