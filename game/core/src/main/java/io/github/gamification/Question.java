c43f6e6dd6df75e6110e26b823cbc2f527e5c17d
Adrian351lang <177791808+Adrian351lang@users.noreply.github.com>
2025-11-05 02:10:35 -0300

    feat: started Question class

---
+++ game/core/src/main/java/io/github/gamification/Question.java
@@ -0,0 +1,13 @@
+package io.github.gamification;
+
+public class Question {
+	private String question;
+	private String[] options;
+	private int answer;
+
+	public Question(String question, String[] options, int answer) {
+		this.question = question;
+		this.options = options;
+		this.answer = answer;
+	}
+}
