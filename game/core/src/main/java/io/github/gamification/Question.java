7f380c7b584d713076d6a74c545c7d28b66ec2ac
Adrian351lang <177791808+Adrian351lang@users.noreply.github.com>
2025-11-05 02:25:16 -0300

    feat: added verification for valid creation of question

--- game/core/src/main/java/io/github/gamification/Question.java
+++ game/core/src/main/java/io/github/gamification/Question.java
@@ -8,6 +8,11 @@ public class Question {
 	public Question(String question, String[] options, int answer) {
 		this.question = question;
 		this.options = options;
-		this.answer = answer;
+		if (answer < options.length)
+			this.answer = answer;
+		else {
+			System.out.printf("Error: answer of question was set as %d, but only %d options were given. Aborting...\n", answer + 1, options.length);
+			System.exit(1);
+		}
 	}
 }
